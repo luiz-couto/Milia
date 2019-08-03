68a0c202af3a31f28e26158264535e6624ecdf34
Luiz <luiz.felipe.556@outlook.com>
2019-08-03 11:56:29 -0300

    Created spendplan page structure

---
+++ Milia/src/pages/SpendPlan/SpendModal.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Modal, View, Text, TouchableOpacity, ImageBackground, Image, TextInput } from 'react-native';
+import { openDatabase } from 'react-native-sqlite-storage';
+import styles from './styles';
+
+let db = openDatabase('inc_list','1.0','Income List', -1)
+
+class SpendModal extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            isVisible: true,
+            spendName: '',
+            spendValue: '',
+        }
+    }
+
+    closeModal() {
+        console.log('passou aqui');
+        this.setState({ isVisible: false }, () => {
+            setTimeout(this.props.closeModal, 500);
+        });
+    }
+    
+    saveData() {
+        let {
+            spendName,
+            spendValue
+        } = this.state
+        
+        console.log(spendName);
+        console.log(spendValue);
+
+        db.transaction(function(tx){
+            tx.executeSql('INSERT INTO spend_table (spend_name,spend_value) VALUES(?,?)',[spendName,spendValue])
+        })
+
+        this.closeModal();
+    }
+
+    render(){
+        
+        const navigation = this.props
+        let { 
+            isVisible,
+            spendName,
+            spendValue,
+        } = this.state
+
+        return(
+                <Modal style={styles.modal_container} 
+                animationType="slide"
+                visible={isVisible}
+                onRequestClose={ () => {this.closeModal()}}
+                > 
+                <TouchableOpacity onPress={() => {this.closeModal()}}>
+                    <Text>Cancelar</Text>
+                </TouchableOpacity>
+
+                <TextInput
+                style={{ height: 70, borderColor: 'gray', borderWidth: 2}}
+                onChangeText={(spendName) => { this.setState ({ spendName })}}
+                value={spendName}
+                >
+                </TextInput>
+                
+                <TextInput
+                style={{ height: 70, borderColor: 'gray', borderWidth: 2}}
+                onChangeText={(spendValue) => { this.setState ({ spendValue })}}
+                value={spendValue}
+                ></TextInput>
+
+                <TouchableOpacity onPress={() => {this.saveData()}}>
+                    <Text>Adicionar</Text>
+                </TouchableOpacity>
+                    
+                </Modal>
+        );
+    }
+}
+
+export default SpendModal;
--- Milia/src/pages/SpendPlan/index.js
+++ Milia/src/pages/SpendPlan/index.js
@@ -3,11 +3,136 @@ import { View, Text, TouchableOpacity, ImageBackground, Image } from 'react-nati
 import { openDatabase } from 'react-native-sqlite-storage';
 
 import { ScrollView } from 'react-native-gesture-handler';
+import SpendModal from './SpendModal';
+import SpendItem from './SpendItem/index';
 
-class SpendPlan extends React.Component{
+let db = openDatabase('inc_list','1.0','Income List', -1)
+
+class SpendPlan extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            value: '',
+            incomeTotal: 0,
+            spendArray: [],
+            showModal: false,
+        }
+    }
+
+    componentDidMount(){
+        db.transaction(function(tx){
+            tx.executeSql('CREATE TABLE IF NOT EXISTS spend_table(spend_id INTEGER PRIMARY KEY AUTOINCREMENT,spend_name TEXT, spend_value TEXT)')
+        })
+        //this.cleanAllData()
+        this.showData()    
+    }
+
+    showData(){
+        let result = new Promise((resolve,reject) => {
+            
+            db.transaction(function(tx){
+            tx.executeSql('SELECT * FROM spend_table',[],(tx,resultado) => {
+                resolve(resultado)
+                
+                
+            })
+        },null)})
+
+        result.then((resultado) => {
+            
+            rows = resultado.rows
+            len = resultado.rows.length
+            
+            this.state.spendArray = []
+
+            if(len==0){
+                this.setState({spendArray: this.state.spendArray})
+            }
+            
+            let total = 0;
+
+            for(i=0;i<len;i++){
+
+                name = rows.item(i).spend_name;
+                value = rows.item(i).spend_value;
+                total = total + Number(value);
+                console.log(total)
+
+                this.state.spendArray.push({
+
+                    'name': name + ';.;' + String(value)
+                    
+                });
+
+
+                this.setState({spendArray: this.state.spendArray, incomeTotal: total})
+                
+                
+            }
+
+            console.log(this.state.spendArray)
+    
+        });
+    }
+
+    cleanAllData(){
+
+        db.transaction(function(tx){
+            tx.executeSql('DELETE FROM spend_table', [], (tx, resultado) =>{
+                
+                console.log('Item deleted')
+
+            })
+        })
+
+        this.showData()
+
+    }
+
+    openModal = () => {
+        this.setState({ showModal: true });
+    }
+
+    closeModal = () => {
+        console.log("PASSOU AQUI");
+        this.setState({ showModal: false },  () => {
+            this.showData();
+        }
+        );
+    }
+    
+    
     render(){
-        return(
-            <Text>Oláa Miliaa</Text>
+
+        const navigation = this.props;
+        const {
+            spendArray,
+            showModal,
+            incomeTotal
+        } = this.state
+        let spends = spendArray.map((val, key) =>{
+            return <SpendItem key={key} val={val}
+                    navigation = {navigation} />
+        })
+        return (
+            <>
+                <View>
+                    <TouchableOpacity onPress={this.openModal}>
+                        <Text>Adicionar</Text>
+                    </TouchableOpacity>
+                    <ScrollView>
+                        {spends}
+                    </ScrollView>
+                </View>
+                {showModal && (
+                    <SpendModal
+                    navigation={navigation}
+                    closeModal={this.closeModal}
+                    />
+                )}
+            </>
         );
     }
 }
