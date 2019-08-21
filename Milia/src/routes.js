c7cec8b64ed72a8b1f684f4f0bb48b5bac42ff7d
Luiz <luiz.felipe.556@outlook.com>
2019-08-21 14:51:22 -0300

    added icons to bottom navigator

--- Milia/src/routes.js
+++ Milia/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Component, Text } from 'react-native';
 import { Root } from "native-base";
+import { Icon } from 'react-native-elements'
 import { createStackNavigator, createAppContainer,createDrawerNavigator,
 DrawerItems, createBottomTabNavigator} from 'react-navigation';
 
@@ -16,10 +17,38 @@ const TabNavig = createBottomTabNavigator(
         ISPend: { screen: ISpend }
 
     },{
+
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, horizontal, tintColor }) => {
+              const { routeName } = navigation.state;
+              //let IconComponent = Ionicons;
+              let iconName;
+              let type;
+              if (routeName === 'Income') {
+                iconName = 'spa';
+                type = 'material';
+              } else if (routeName === 'SpendPlan') {
+                iconName = 'receipt';
+                type = 'material';
+              } else if (routeName === 'ISPend'){
+                iconName = 'toll';
+                type = 'material';
+              }
+              // You can return any component that you like here!
+              return <Icon name={iconName} type={type} size={30} color={tintColor} />;
+            },
+        }),
+
+
         tabBarOptions: {
-            activeTintColor: 'white',
+            activeTintColor: 'yellow',
             inactiveTintColor: 'white',
-            style: {backgroundColor: 'rgb(252,95,95)'}
+            showLabel: false,
+            style: {
+                backgroundColor: 'rgb(252,95,95)',
+                height: 65,
+            }
+            
         },
     }
 )
